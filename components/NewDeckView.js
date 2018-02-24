7fe1c0cf5fd0c3aaebfeba330ed59f10a55b2c16
TudorTacal <tudortacal@gmail.com>
2018-02-24 01:04:08 +0000

    feat: Add submit button and input text field on NewDeckView
    - finish the stylying
    - next to save the new deck in decks

--- components/NewDeckView.js
+++ components/NewDeckView.js
@@ -6,30 +6,71 @@ import { white, black, gray }from "../utils/colors";
 import * as api from "../models/api";
 
 class NewDeckView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            deckTitle: "Deck title",
+         };
+         this.createNewDeckAsync = this.createNewDeckAsync.bind(this);
+    }
+
+    createNewDeckAsync = (deckTitle) => {
+        console.log('hello')
+    }
+
     render() {
         return (
-            <View style={{flex: 1}}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <View style={styles.blackLine}/>
+            <React.Fragment>
+                <View style={{alignItems: 'flex-end', marginBottom: 50}}>
                     <View style={[styles.deckHeader]}>
                         <Text style={{fontSize: 18, padding: 20}}>NEW DECK</Text>
                     </View>
                 </View>
-            </View>
+                <View style={{alignItems: 'center', marginBottom: 25}}>
+                    <Text style={{fontSize: 50, fontWeight: 'bold', textAlign: "center"}}>
+                        What is the title of your new deck?
+                    </Text>
+                </View>
+                <View style={{flex: 1, alignItems: 'center'}}>
+                    <TextInput 
+                        style={[styles.inputField, {marginTop: 25}]}
+                        value={this.state.deckTitle}
+                        onChangeText={(deckTitle) => {this.setState({deckTitle})}}/>
+                    <TouchableOpacity 
+                        style={styles.submitButton}
+                        onPress={()=> this.createNewDeckAsync(this.state.deckTitle)}>
+                        <Text style={{color: white, fontSize: 24}}>Submit</Text>
+                    </TouchableOpacity>
+                </View>
+            </React.Fragment>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    blackLine: {
-        borderTopColor: 'black',
-        borderTopWidth: 1,
-    },
     deckHeader: {
         width: Dimensions.get('window').width / 2,
         borderBottomColor: '#FFFF00',
         borderBottomWidth: 3,
-        alignItems: 'flex-end',
+        alignItems: 'center',
+    },
+    inputField: {
+        borderColor: black,
+        borderWidth: 2,
+        borderRadius: 5,
+        height: 45,
+        width: Dimensions.get('window').width / 1.1,
+        marginBottom: 45,
+        paddingLeft: 10
+    },
+    submitButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 70,
+        width: 180,
+        backgroundColor: black,
+        borderRadius: 7,
     },
   });
 
